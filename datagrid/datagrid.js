156392cf3e481730317c57735d5f14fd543aa127
王小龙 <1036199213@qq.com>
2017-05-11 23:44:03 +0800

    g

--- datagrid/datagrid.js
+++ datagrid/datagrid.js
@@ -1,49 +1,53 @@
 import React from 'react';
 import {
-	Table,
-	Icon
+    Table,
+    Icon
 } from 'antd';
 
 class DataGrid extends extends React.Component {
 
-		get columns() {
-			let _columns = [{
-				title: 'Name',
-				dataIndex: 'name',
-				key: 'name',
-				render: text => < a href = "#" > {
-					text
-				} < /a>,
-			}, {
-				title: 'Age',
-				dataIndex: 'age',
-				key: 'age',
-			}, {
-				title: 'Address',
-				dataIndex: 'address',
-				key: 'address',
-			}, {
-				title: 'Action',
-				key: 'action',
-				render: (text, record) => ( < span >
-					< a href = "#" > Action 一 {
-						record.name
-					} < /a> < span className = "ant-divider" / >
-					< a href = "#" > Delete < /a> < span className = "ant-divider" / >
-					< a href = "#"
-					className = "ant-dropdown-link" >
-					More actions < Icon type = "down" / >
-					< /a> < /span >
-				),
-			}];
-			return _columns;
-		}
+        get columns() {
+            let _columns = [{
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+                render: text => < a href = "#" > {
+                    text
+                } < /a>,
+            }, {
+                title: 'Age',
+                dataIndex: 'age',
+                key: 'age',
+            }, {
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address',
+            }, {
+                title: 'Action',
+                key: 'action',
+                render: (text, record) => ( < span >
+                    <
+                    a href = "#" > Action 一 {
+                        record.name
+                    } < /a> < span className = "ant-divider" / >
+                    <
+                    a href = "#" > Delete < /a> < span className = "ant-divider" / >
+                    <
+                    a href = "#"
+                    className = "ant-dropdown-link" >
+                    More actions < Icon type = "down" / >
+                    <
+                    /a> < /span >
+                ),
+            }];
+            return _columns;
+        }
 
-		render() {
-			return ( < span > {
-					this.name
-				} < /span>);
-			}
-		}
+        render() {
+            return ( < span > {
+                    this.name
+                } < /span>);
+            }
+        }
 
-		export default DataGrid;
+        export default DataGrid;
