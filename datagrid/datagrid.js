b5c81c28bf1e81cb80a4ddb839f341bff902be7b
王小龙 <1036199213@qq.com>
2017-05-12 16:51:00 +0800

    格式化

--- datagrid/datagrid.js
+++ datagrid/datagrid.js
@@ -1,53 +1,50 @@
 import React from 'react';
-import {
-    Table,
-    Icon
-} from 'antd';
+import { Table, Icon } from 'antd';
 
 class DataGrid extends extends React.Component {
 
-        get columns() {
-            let _columns = [{
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name',
-                render: text => < a href = "#" > {
-                    text
-                } < /a>,
-            }, {
-                title: 'Age',
-                dataIndex: 'age',
-                key: 'age',
-            }, {
-                title: 'Address',
-                dataIndex: 'address',
-                key: 'address',
-            }, {
-                title: 'Action',
-                key: 'action',
-                render: (text, record) => ( < span >
-                    <
-                    a href = "#" > Action 一 {
-                        record.name
-                    } < /a> < span className = "ant-divider" / >
-                    <
-                    a href = "#" > Delete < /a> < span className = "ant-divider" / >
-                    <
-                    a href = "#"
-                    className = "ant-dropdown-link" >
-                    More actions < Icon type = "down" / >
-                    <
-                    /a> < /span >
-                ),
-            }];
-            return _columns;
-        }
+    get columns() {
+        let _columns = [{
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: text => <a href="#">{text}</a>
+        }, {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age'
+        }, {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address'
+        }, {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <span>
+                    <a href="#">
+                        Action 一 {record.name}
+                    </a>
+                    < span className="ant-divider" />
+                    <a href="#">
+                        Delete
+                    </a>
+                    <span className="ant-divider" />
+                    <a href="#" className="ant-dropdown-link">
+                        More actions < Icon type="down" />
+                    </a>
+                    
+                </span >
+            )
+        }];
+        return _columns;
+    }
 
-        render() {
-            return ( < span > {
-                    this.name
-                } < /span>);
-            }
-        }
-
-        export default DataGrid;
+    render() {
+        return (
+            <span>
+                {this.name}
+            </span>
+        );
+    }
+}
