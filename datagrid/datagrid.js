6c38f69e6d4ee565d786500db614fdf444027f48
王小龙 <1036199213@qq.com>
2017-05-12 17:53:57 +0800

    sss

--- datagrid/datagrid.js
+++ datagrid/datagrid.js
@@ -21,9 +21,22 @@ class DataGrid extends extends React.Component {
     }
 
     render() {
+        const dataSource = [{
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号'
+        }, {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号'
+        }];
+
         return (
             <span>
                 {this.name}
+                <Table dataSource={dataSource} columns={this.columns} />
             </span>
         );
     }
