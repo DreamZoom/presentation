568c082079c072f6ce6407fb493a8e8386e90a37
王小龙 <1036199213@qq.com>
2017-05-18 21:38:40 +0800

    大大大大

--- datagrid/datagrid.js
+++ datagrid/datagrid.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Table, Icon } from 'antd';
+import { Table, Icon, Button, Popconfirm } from 'antd';
 
 class DataGrid extends React.Component {
 
-    state={
-        metadata:[],
-        apis:{},
-        data:[]
+    state = {
+        metadata: [],
+        apis: {},
+        data: [{
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号'
+        }, {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号'
+        }]
     }
 
     get columns() {
@@ -22,34 +32,84 @@ class DataGrid extends React.Component {
             title: '住址',
             dataIndex: 'address',
             key: 'address',
+        }, {
+            title: "操作",
+            dataIndex: 'operation',
+            key: 'operation',
+            render: (text, record, index) => {
+
+                const editable = record.__editable || false;
+                console.log(editable);
+                return (
+                    <div className="editable-row-operations">
+                        {
+                            editable ?
+                                <span>
+                                    <Button onClick={() => this.handleUpdateOrCancel(record, index)}>保存</Button>
+                                    <Popconfirm title="Sure to cancel?" onConfirm={() => this.handleUpdateOrCancel(record, index, true)}>
+                                        <Button>取消</Button>
+                                    </Popconfirm>
+                                </span>
+                                :
+                                <span>
+                                    <Button onClick={() => this.handleEditRecord(record, index)}>编辑</Button>
+                                </span>
+                        }
+                    </div>
+                );
+            }
         }];
         return _columns;
     }
 
-    get toolboxs(){
-        
+    handleEditRecord(record, index) {
+        record.__editable = true;
+        this.setState({
+            ...this.state
+        });
     }
 
+    handleUpdateOrCancel(record, index, cancel) {
+        if (cancel) {
+            record.__editable = false;
+            this.setState({
+                ...this.state
+            });
+        }
+        else {
+            record.__editable = false;
+            this.setState({
+                ...this.state
+            });
+
+        }
+
+    }
+
+    handleNewRecord() {
+
+        this.state.data.push({
+            key: '' + (this.state.data.length + 1),
+            name: '',
+            age: 0,
+            address: ''
+        });
+        this.setState({
+            ...this.state
+        });
+    }
+
+
     render() {
-        
-        const dataSource = [{
-            key: '1',
-            name: '胡彦斌',
-            age: 32,
-            address: '西湖区湖底公园1号'
-        }, {
-            key: '2',
-            name: '胡彦祖',
-            age: 42,
-            address: '西湖区湖底公园1号'
-        }];
+
+
 
         return (
             <div>
                 <div className="toolbox">
-                    {this.toolboxs}
+                    <Button onClick={() => { this.handleNewRecord() }}>新建</Button>
                 </div>
-                <Table dataSource={dataSource} columns={this.columns} />
+                <Table dataSource={this.state.data} columns={this.columns} />
             </div>
         );
     }
